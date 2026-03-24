78431952a066427f8fbd26b2163de1f4a8a65382
Shubham Shrestha <shrestha.subham.60@gmail.com>
2026-03-24 23:08:12 +0545

    fix: variabe  fix

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,11 @@
 variable "kubernetes_config_path" {
   description = "Path to the kubeconfig file"
-  type        = "string"
+  type        = string
   default     = "~/.kube/config"
 }
 
 variable "monitoring_namespace" {
   description = "Namespace for monitoring tools"
-  type        = "string"
+  type        = string
   default     = "monitoring"
 }
